4b1e8861e676476ce9c515c129bcbd5f5c4b3853
riccardoporreca <riccardo.porreca@mirai-solutions.com>
2020-03-21 18:27:26 +0100

    Custom javascript to restore responsive gallery container width for certain classes.

---
+++ main.js
@@ -0,0 +1,4 @@
+/* Responsive gallery container width for certain classes */
+$(document).ready(function () {
+  $('.gallery-embed-script, .gallery-embed-html').addClass('main-container container-fluid');
+});
